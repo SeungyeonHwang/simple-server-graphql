fc64fbe2e27a45314e96750e53d43e070fb1cf42
ファン <syhwang.web@gmail.com>
2021-05-03 22:37:07 +0900

    #first commit

---
+++ src/main/java/com/example/simpleservergraphql/SimpleServerGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleservergraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleServerGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServerGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleservergraphql/SimpleServerGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleservergraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServerGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
